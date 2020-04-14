944d62055b1ca638f9be7ffa65b698f584c5eca6
Konstantin-GoStartups <konstantin@gostartups.net>
2020-04-14 15:36:14 +0300

    first and final commit

---
+++ Sources/App/Routes/ToDoRoutes.swift
@@ -0,0 +1,16 @@
+// 
+// ToDoRoutes.swift
+//  App
+//
+//  Created by Konstantin Kostadinov on 14.04.20
+//  Copyright (C) 2012-2020 by Activbody, Inc. All Rights Reserved.
+
+//  Data contained herein is proprietary information of Activbody, Inc,
+//  which shall be treated confidentially and shall not be used by anyone,
+//  furnished to third parties or made public without prior written permission by Activbody, Inc.
+
+//  The Activbody(TM), Activ5(TM) and TAO brands and products are fully protected
+//  by international trademark, copyright and patent laws.
+//  All rights reserved 2008-2020 Activbody, Inc.
+
+import Foundation
